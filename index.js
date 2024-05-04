ebc0ccc9d31b1b68e5bb227a310e2a8ca8c2bae3
raharinjatovo <raharinjatovo.a@zurcher.edu.mg>
2024-05-04 16:37:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+// a basic usage of gemini pro in apis node js 
+// require express for rooting
+const express = require('express')
+const app = express()
+
+app.get('/apis',async(req,res)=>{
+ 
+})
+
+app.listen(8000,()=>{
+    console.log('server is running on port 8000')
+})
